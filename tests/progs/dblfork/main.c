203320736bd74e4abdc2eb12d9782219a8abc35c
Cauê Waneck <waneck@gmail.com>
2015-07-25 02:15:09 -0300

    Cleanup: Better handling exit codes and waitpid

--- tests/progs/dblfork/main.c
+++ tests/progs/dblfork/main.c
@@ -4,32 +4,44 @@
 #include <string.h>
 #include <unistd.h>
 #include <err.h>
+#include <sys/wait.h>
 
 int main()
 {
 	printf(" -> parent\n");
-	bool parent = true;
 	pid_t child = fork();
-	printf(" -> fork %d\n",child);
 	if (child < 0) err(1,"fork");
 	if (!child)
 	{
-		parent = false;
 		printf(" -> child 1\n");
 		if (!fork())
 		{
 			printf(" -> child 2\n");
 			// let's make now a different syscall to see if it gets traced as well
-			FILE *f = fopen("/tmp/test", "w");
-			fputs("file", f);
-			fclose(f);
+			sleep(1);
+			return 10;
+		} else {
+			while(true)
+			{
+				int status;
+				int x = waitpid(0, &status, 0);
+				if (WIFEXITED(status))
+					return WEXITSTATUS(status);
+			}
 		}
 	}
 
-	int status;
-	if (parent)
-		waitpid(-1,&status,0);
-	if (parent)
-		printf(" -> exiting\n");
+	while (true)
+	{
+		int status;
+		int x = waitpid(0,&status,0);
+		if (x < 0) err(1, "waitpid");
+		if (WIFEXITED(status))
+		{
+			printf(" -> exiting\n");
+			return WEXITSTATUS(status);
+		}
+	}
+
 	return 0;
 }
--- trace.c
+++ trace.c
@@ -39,7 +39,7 @@ static void get_syscall(pid_t child, long *normalized_id, char **name)
 	*normalized_id = syscall;
 }
 
-static int wait_for_seccomp_or_attach(pid_t child, int *child_count, pid_t *cur_child)
+static int wait_for_seccomp_or_attach(pid_t child, int *child_count, pid_t *cur_child, int *exit_code)
 {
 	while(true)
 	{
@@ -50,7 +50,7 @@ static int wait_for_seccomp_or_attach(pid_t child, int *child_count, pid_t *cur_
 			return *cur_child;
 		if (WIFSTOPPED(status))
 		{
-			// SIGTRAP - ptrace trapped the program somehow
+			// SIGTRAP - ptrace trapped the program
 			if (WSTOPSIG(status) == SIGTRAP)
 			{
 				int ev = ((status >> 8) & ~SIGTRAP) >> 8;
@@ -63,35 +63,32 @@ static int wait_for_seccomp_or_attach(pid_t child, int *child_count, pid_t *cur_
 						return 1;
 					case PTRACE_EVENT_EXIT:
 						// a child has exited; we'll check if all children have left later
-						(*child_count)--;
 						break;
 					case PTRACE_EVENT_CLONE:
 					case PTRACE_EVENT_FORK:
 					case PTRACE_EVENT_VFORK:
+						// we've just created another process; it will be trapped again, with waitpid
+						// return its own pid
 						(*child_count)++;
-						pid_t gchild;
-						CHECK_POSIX(ptrace(PTRACE_GETEVENTMSG, *cur_child, 0, &gchild));
-						/* printf("new process: %d\n", gchild); */
 						break;
-					/* case PTRACE_EVENT_EXEC: */
-					/* 	printf("exec\n");; */
-					/* 	CHECK_POSIX(ptrace(PTRACE_SETOPTIONS, *cur_child, 0, PTRACE_FLAGS)); */
-					/* 	break; */
 					default:
 						ERRX("Unexpected SIGTRAP event %x", ev);
 				}
 				CHECK_POSIX(ptrace(PTRACE_CONT, *cur_child, 0, 0));
 			} else if (WSTOPSIG(status) == SIGSTOP) {
+				// trace the new child
 				CHECK_POSIX(ptrace(PTRACE_SETOPTIONS, *cur_child, 0, PTRACE_FLAGS));
 				CHECK_POSIX(ptrace(PTRACE_CONT, *cur_child, 0, 0));
 			} else {
-				/* ERRX("Unexpected stop status: %x (%d)", status, WSTOPSIG(status)); */
-				printf("Unexpected stop status: %x (%d)\n", status, WSTOPSIG(status));
+				// pass the signal
+				CHECK_POSIX(ptrace(PTRACE_CONT, *cur_child, 0, WSTOPSIG(status)));
 			}
 		} else if (WIFEXITED(status)) {
+			if (*cur_child == child)
+				*exit_code = WEXITSTATUS(status);
 			(*child_count)--;
 		} else {
-				ERRX("Unexpected status: %x", status);
+			ERRX("Unexpected status: %x", status);
 		}
 		if (*child_count < 0)
 			return 0;
@@ -107,11 +104,11 @@ static int trace_process(pid_t child, char *output)
 	CHECK_POSIX(ptrace(PTRACE_SETOPTIONS, child, 0, PTRACE_FLAGS));
 	CHECK_POSIX(ptrace(PTRACE_CONT, child, 0, 0));
 
-	int child_count = 0;
+	int child_count = 0, exit_code = 0;
 	while(true)
 	{
 		pid_t cur_child;
-		int ev = wait_for_seccomp_or_attach(child, &child_count, &cur_child);
+		int ev = wait_for_seccomp_or_attach(child, &child_count, &cur_child, &exit_code);
 		CHECK_POSIX(ev);
 		if (!ev) break;
 
@@ -126,7 +123,7 @@ static int trace_process(pid_t child, char *output)
 		CHECK_POSIX(ptrace(PTRACE_CONT, cur_child, 0, 0));
 	}
 
-	return 0;
+	return exit_code;
 }
 
 static int child_process(int argc, char **argv)
